556a44281da9cf56bbe515226ec4d6f819703ea1
Tripta Gupta <gupta.tripta@gmail.com>
2014-04-02 09:58:07 -0700

    initial commit

---
+++ WWCMentorship/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WWCMentorship
+//
+//  Created by Tripta Gupta on 4/2/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WWCMentorship/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WWCMentorship
+//
+//  Created by Tripta Gupta on 4/2/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
