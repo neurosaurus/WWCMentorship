e30b5d92984a8baa64ef740467b5a0009b9a038f
Tripta Gupta <gupta.tripta@gmail.com>
2014-04-08 22:13:38 -0700

    mentorlistcontroller

---
+++ WWCMentorship/MentorListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MentorListViewController.h
+//  WWCMentorship
+//
+//  Created by Tripta Gupta on 4/8/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MentorListViewController : UIViewController
+
+@end
---
+++ WWCMentorship/MentorListViewController.m
@@ -0,0 +1,38 @@
+//
+//  MentorListViewController.m
+//  WWCMentorship
+//
+//  Created by Tripta Gupta on 4/8/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import "MentorListViewController.h"
+
+@interface MentorListViewController ()
+
+@end
+
+@implementation MentorListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
