85d4aac1fb3e8add456a56ecb7085c5e74c74789
Tripta Gupta <gupta.tripta@gmail.com>
2014-04-14 22:26:06 -0700

    pfloginviewcontrollerdelegate

--- WWCMentorship/LoginViewController.m
+++ WWCMentorship/LoginViewController.m
@@ -51,9 +51,41 @@
         [loginViewController setSignUpController:signupViewController];
         
         //Present the login view controller
-        [self presentedViewController:loginViewController animated:YES completion:NULL];
+        [self presentViewController:loginViewController animated:YES completion:NULL];
     }
 }
+
+- (BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password
+{
+    // Check if both fields are completed
+    if (username && password && username.length != 0 && password.length != 0) {
+        return YES; // Begin login process
+    }
+    
+    [[[UIAlertView alloc] initWithTitle:@"Missing Information"
+                                message:@"Make sure you fill out all of the information!"
+                               delegate:nil
+                      cancelButtonTitle:@"ok"
+                      otherButtonTitles:nil] show];
+    return NO; // Interrupt login process
+}
+
+// Sent to the delegate when a PFUser is logged in.
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+// Sent to the delegate when the log in attempt fails.
+- (void)logInViewController:(PFLogInViewController *)logInController didFailToLogInWithError:(NSError *)error {
+    NSLog(@"Failed to log in...");
+}
+
+// Sent to the delegate when the log in screen is dismissed.
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
